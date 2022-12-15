a4a98b90c74b55d9a4790358301bab536132a10b
Wiger <120435282+wigerwei@users.noreply.github.com>
2022-12-15 23:09:02 +0800

    Create dag-maker.py

---
+++ dag/dag-maker.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+'''
+@FileName    : dag-maker.py
+@Description : Make a DAG of transactions according their statement.
+@Date        : 2022/12/15 23:07:40
+@Author      : Wiger
+@version     : 1.0
+'''
