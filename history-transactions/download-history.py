6917a21ade0b4189ef67814eae92cbc7a3340583
Wiger <120435282+wigerwei@users.noreply.github.com>
2022-12-15 09:48:29 +0800

    Update download-history.py

--- history-transactions/download-history.py
+++ history-transactions/download-history.py
@@ -11,7 +11,7 @@ import pickle
 from web3 import Web3, HTTPProvider
 
 # Instantiate a web3 remote provider
-w3 = Web3(HTTPProvider('YOUR_QUICKNODE_HTTP_ENDPOINT'))
+w3 = Web3(HTTPProvider('https://eth-mainnet.g.alchemy.com/v2/QL0Uqa-Z_QgMgBBKDFOUB8zoYx7q5Lg-'))
 
 # Request the latest block number
 ending_blocknumber = w3.eth.blockNumber
@@ -20,31 +20,36 @@ ending_blocknumber = w3.eth.blockNumber
 starting_blocknumber = ending_blocknumber - 100 
 
 # Filter through blocks and look for transactions involving this address
-blockchain_address = "INPUT_A_VALID_ETHEREUM_ADDRESS"
+blockchain_address = ""
 
 # Create an empty dictionary we will add transaction data to
 tx_dictionary = {}
 
-def getTransactions(start, end, address):
+def getTransactions(start, end, address=""):
     """
     @Description  : Doanload transactions involving this address from start block to end block.
     @Params       :
         start: Start block
         end: End block
-        address: Filter address
+        address: Filter address, 
     @Returns      : None
     """
-    print(f"Started filtering through block number {start} to {end} for transactions involving the address - {address}...")
+    print(f"Started filtering through block number {start} to {end} for transactions involving the address: {address}...")
     for x in range(start, end):
         block = w3.eth.getBlock(x, True)
         for transaction in block.transactions:
-            if transaction['to'] == address or transaction['from'] == address:
-                with open("transactions.pkl", "wb") as f:
+            if address == "":
+                with open("transactions.txt", "wb") as f:
+                    hashStr = transaction['hash'].hex()
+                    tx_dictionary[hashStr] = transaction
+                    pickle.dump(tx_dictionary, f)
+                f.close()
+            elif transaction['to'] == address or transaction['from'] == address:
+                with open("transactions.txt", "wb") as f:
                     hashStr = transaction['hash'].hex()
                     tx_dictionary[hashStr] = transaction
                     pickle.dump(tx_dictionary, f)
                 f.close()
     print(f"Finished searching blocks {start} through {end} and found {len(tx_dictionary)} transactions")
-    
 
 getTransactions(starting_blocknumber, ending_blocknumber, blockchain_address)
