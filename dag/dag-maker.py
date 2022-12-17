b69ef2e864d98c2d0b25d0f458ef966ab84d971c
Wiger <120435282+wigerwei@users.noreply.github.com>
2022-12-17 19:32:15 +0800

    update state-explorer and dag-maker

--- dag/dag-maker.py
+++ dag/dag-maker.py
@@ -6,3 +6,30 @@
 @Author      : Wiger
 @version     : 1.0
 '''
+# Import dependencies
+import networkx as nx
+import matplotlib.pyplot as plt
+
+G = nx.DiGraph()
+
+G.add_node(1, amount=100, sender='Alice', receiver='Bob')
+G.add_node(2, amount=50, sender='Charlie', receiver='Alice')
+G.add_node(3, amount=75, sender='Bob', receiver='Charlie')
+
+G.add_edge(1, 2)
+G.add_edge(1, 3)
+G.add_edge(2, 3)
+
+# Draw the graph
+nx.draw(G, with_labels=True)
+plt.show()
+
+# Function
+def make_dag(txList, stateList):
+    """
+    @Description  : A function of building a DAG of transactions
+    @Params       :
+        txList: Transaction list.
+        stateList: Statement of transactions list.
+    @Returns      : A full DAG.
+    """
---
+++ dag/state-explorer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+'''
+@FileName    : state-explorer.py
+@Description :
+@Date        : 2022/12/17 19:31:51
+@Author      : Wiger
+@version     : 1.0
+'''
