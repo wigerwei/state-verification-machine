44cc9f6a55cdffc2bca7fc7d1880e7eba7715558
Wiger <120435282+wigerwei@users.noreply.github.com>
2022-12-18 23:24:53 +0800

    Create example-vis3.py

---
+++ example/example-vis3.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+'''
+@FileName    : example3.py
+@Description : A example of DAG graph visualization.
+@Date        : 2022/12/18 23:18:20
+@Author      : Wiger
+@version     : 1.0
+'''
+# Import the required libraries
+import networkx as nx
+import matplotlib.pyplot as plt
+import random
+
+# Create an empty graph
+G = nx.DiGraph()
+
+# Add 10 nodes
+for i in range(70):
+    G.add_node(i)
+
+# Add directed edges
+for i in range(90):
+    G.add_edge(random.randint(0,70), random.randint(0,70))
+
+# Draw the graph
+nx.draw(G, with_labels=True)
+plt.show()
