c20676d59de139166f1df370c32e28f057beca62
Wiger <120435282+wigerwei@users.noreply.github.com>
2022-12-18 23:14:01 +0800

    Create config.py

---
+++ node/config.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+'''
+@FileName    : config.py
+@Description : Set the node according to config.
+@Date        : 2022/12/18 23:13:41
+@Author      : Wiger
+@version     : 1.0
+'''
