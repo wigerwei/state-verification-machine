5c43715169faa4d94f2ce5d9479b7f5167d65019
Wiger <120435282+wigerwei@users.noreply.github.com>
2022-12-20 13:03:50 +0800

    Create node-simulator.py

---
+++ node/node-simulator.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+'''
+@FileName    : node-simulator.py
+@Description : Node simulator in python.
+@Date        : 2022/12/20 13:03:35
+@Author      : Wiger
+@version     : 1.0
+'''
