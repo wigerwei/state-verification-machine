dd797ec94db15d12d3c7c72084b4fd64b757f537
Wiger <120435282+wigerwei@users.noreply.github.com>
2023-01-01 23:33:08 +0800

    Update pre-order.py

--- pre-order-python/pre-order.py
+++ pre-order-python/pre-order.py
@@ -7,11 +7,18 @@
 @version     : 1.0
 '''
 
-def call_evm(trx_list, ):
+def call_evm(trx_list, trx_type, call_type):
     """
     @Description  : Call EVM simulator with this function
     @Params       : trx_list: Transactions list.
     @Returns      : EVM runs well/wrong.
+    """
+    
+def evm_parse(evm_type):
+    """
+    @Description  : Parse the result of EVM output.
+    @Params       :
+    @Returns      :
     """
     
     
